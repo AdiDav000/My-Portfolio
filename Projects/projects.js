27cd5eb19f7edf72ceea25f68bf4915f11bf4c30
AdiDav000 <adidavar000@gmail.com>
2024-03-03 16:46:51 +0530

    Added social media handles

---
+++ Projects/projects.js
@@ -0,0 +1,10 @@
+document.getElementById("linkedin").addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/in/aditya-davar-1362021b3/";
+});
+document.getElementById("gmail").addEventListener("click", function() {
+    var mailtoLink = 'mailto:' + "adidavar000@gmail.com";
+    window.location.href = mailtoLink;
+});
+document.getElementById("instagram").addEventListener("click", function() {
+    window.location.href = "https://www.instagram.com/adidav000/";
+});
