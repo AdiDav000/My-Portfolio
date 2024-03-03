4c49ea7521b50a5ebaeb113979953603479b2406
AdiDav000 <adidavar000@gmail.com>
2024-03-03 22:48:08 +0530

    added button animations

--- Projects/DrumKit/index.js
+++ Projects/DrumKit/index.js
@@ -1,37 +1,11 @@
 for (var i=0;i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
         var instrument = this.innerHTML;
-        if(instrument === "w"){
-            var w=new Audio("./sounds/crash.mp3");
-            w.play();
-        }
-        if(instrument === "a"){
-            var a=new Audio("./sounds/kick-bass.mp3");
-            a.play();
-        }
-        if(instrument === "s"){
-            var s=new Audio("./sounds/snare.mp3");
-            s.play();
-        }
-        if(instrument === "d"){
-            var d=new Audio("./sounds/tom-1.mp3");
-            d.play();
-        }
-        if(instrument === "j"){
-            var j =new Audio("./sounds/tom-2.mp3");
-            j.play();
-        }
-        if(instrument === "k"){
-            var k=new Audio("./sounds/tom-3.mp3");
-            k.play();
-        }
-        if(instrument === "l"){
-            var l=new Audio("./sounds/tom-4.mp3");
-            l.play();
-        }
+        playSound(instrument);
+        playAnimation(instrument);
      });
 }
-document.addEventListener("keydown", function(e){playSound(e.key)});
+document.addEventListener("keydown", function(e){playSound(e.key); playAnimation(e.key);});
 function playSound(key){
     if(key === "w"){
         var w=new Audio("./sounds/crash.mp3");
@@ -61,4 +35,11 @@ function playSound(key){
         var l=new Audio("./sounds/tom-4.mp3");
         l.play();
     } 
+ }
+
+ function playAnimation(key){
+    document.querySelector("."+key).classList.add("pressed");
+    setTimeout(function() {
+        document.querySelector("."+key).classList.remove("pressed");
+    }, 100);
  }
