3072b0b82e7c49ee48b2db50c82437321360ffcf
AdiDav000 <adidavar000@gmail.com>
2024-03-08 23:54:59 +0530

    Social Media website

--- Projects/Capstone_ejs_node/index.js
+++ Projects/Capstone_ejs_node/index.js
@@ -1,14 +1,12 @@
 import express from "express";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-const _dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
-    res.render(_dirname+"/homepage/index.ejs");
+    res.render("index.ejs");
 })
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
