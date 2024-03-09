13f4c12d949512a97c50213230fa6b6f7685e685
AdiDav000 <adidavar000@gmail.com>
2024-03-09 17:05:26 +0530

    social media app ejs

--- Projects/Capstone_ejs_node/index.js
+++ Projects/Capstone_ejs_node/index.js
@@ -1,12 +1,20 @@
 import express from "express";
-
+var posts = [];
 const app = express();
 const port = 3000;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
-    res.render("index.ejs");
+    res.render("index.ejs", {posts: posts});
+})
+
+app.post("/new-post",(req,res)=>{
+    var date = new Date();
+    var datestring = date.toString().slice(3,21);
+    posts.push({title: req.body["post-title"], content: req.body["post-content"], time: datestring});
+    res.render("index.ejs", {posts:posts});
 })
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
